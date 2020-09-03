e2396800f1d0c61edc8080318a0f14927442ef42
barisalbyrak <barisalbayrak34@gmail.com>
2020-09-03 17:32:19 +0300

    updated v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,25 @@
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() >10 ) {
+            $('.navbar').addClass('bg-light');
+        } else {
+            $('.navbar').removeClass('bg-light');
+        }
+    });
+  });
 
 $(function () {
 
     $(window).bind("resize", function () {
         console.log($(this).width())
         if ($(this).width() < 767) {
-            $('div').removeClass('align-items-end').addClass('align-items-center')
+            $('div.ladder').removeClass('align-items-end').addClass('align-items-center')
+            $('div.mixer').removeClass('position-absolute')
+            document.getElementById("spark").src = "/assets/materials/png/half.png";
         } else {
-            $('div').removeClass('align-items-center').addClass('align-items-end')
+            $('div.ladder').removeClass('align-items-center').addClass('align-items-end')
+            $('div.mixer').addClass('position-absolute')
+            document.getElementById("spark").src = "/assets/materials/png/full.png";
         }
     }).trigger('resize');
 })
@@ -26,7 +39,7 @@ function visited(x) {
 
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "50%";
+    document.getElementById("mySidenav").style.width = "65%";
     document.getElementById("mySidenav").style.borderLeft = "2px solid #BFC998";
   }
   
@@ -35,3 +48,17 @@ function openNav() {
     document.getElementById("mySidenav").style.borderLeft = "0px solid #BFC998";
     sidebar = true
   }
+  
+  $(document).ready(function(){
+    $('.arg0_swiper').slick({
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: false,
+    });
+    $('#arg0_swiper--prev').on('click', function() {
+      $('.arg0_swiper').slick('slickPrev');
+    });
+    $('#arg0_swiper--next').on('click', function() {
+      $('.arg0_swiper').slick('slickNext');
+    });
+  });
